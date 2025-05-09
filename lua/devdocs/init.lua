f8f3b2af70ffbe4732e29259e32fb346efc54f2d
Nitai Charan <nitaicharan@gmail.com>
2025-05-09 21:20:35 +0100

    fix(setup): move `ui` and `api` to new table on `setup`

--- lua/devdocs/init.lua
+++ lua/devdocs/init.lua
@@ -13,16 +13,14 @@ return {
   setup = function()
     local registries_request = require("devdocs.infrastructure.requests.registries_request")
     local registries_repository = require("devdocs.infrastructure.repositories.registeries_repository")
-    local documentations_ui = require("devdocs.infrastructure.uis.documentations_ui")
     local lifecycle_usecase = require("devdocs.application.usecases.lifecycle_usecase")
 
     lifecycle_usecase.on_plugin_init(registries_request, registries_repository)
-
-    return {
-      documentations = documentations_ui,
-      api = {
-        documentations = require("devdocs.infrastructure.apis.documentations_api"),
-      }
-    }
-  end
+  end,
+  ui = {
+    documentations = require("devdocs.infrastructure.uis.documentations_ui"),
+  },
+  api = {
+    documentations = require("devdocs.infrastructure.apis.documentations_api"),
+  }
 }
